3d6d22aac1a3275de150fb3e8585356041bc1dbc
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-10-18 19:39:03 -0200

    Initial Commit

---
+++ sulamericaparadiso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sulamericaparadiso
+//
+//  Created by Juan Beleño Díaz on 18/10/16.
+//  Copyright © 2016 Juan Beleño Díaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

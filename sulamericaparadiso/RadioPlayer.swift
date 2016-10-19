204f962dafb039bcba644942d53cceb585889b9e
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-10-19 21:45:51 -0200

    update: functionality is almost done, just rest control the volume

---
+++ sulamericaparadiso/RadioPlayer.swift
@@ -0,0 +1,38 @@
+//
+//  RadioPlayer.swift
+//  sulamericaparadiso
+//
+//  Created by Juan Beleño Díaz on 19/10/16.
+//  Copyright © 2016 Juan Beleño Díaz. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class RadioPlayer {
+    static let sharedInstance = RadioPlayer()
+    private var player = AVPlayer(url: URL(string: "http://evp.mm.uol.com.br/dial-sulamerica/_definst_//dial-sulamerica.sdp/playlist.m3u8")!)
+    private var isPlaying = false
+    
+    func play() {
+        player.play()
+        isPlaying = true
+    }
+    
+    func pause() {
+        player.pause()
+        isPlaying = false
+    }
+    
+    func toggle() {
+        if isPlaying == true {
+            pause()
+        } else {
+            play()
+        }
+    }
+    
+    func currentlyPlaying() -> Bool {
+        return isPlaying
+    }
+}
--- sulamericaparadiso/ViewController.swift
+++ sulamericaparadiso/ViewController.swift
@@ -2,17 +2,68 @@
 //  ViewController.swift
 //  sulamericaparadiso
 //
+// This project was based on the project https://github.com/luciancancescu/sample-radio-app
+//
 //  Created by Juan Beleño Díaz on 18/10/16.
 //  Copyright © 2016 Juan Beleño Díaz. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
+import MediaPlayer
 
 class ViewController: UIViewController {
-
+    
+    //MARK: - Outlets
+    
+    @IBOutlet weak var btnPause: UIButton!
+    @IBOutlet weak var btnPlay: UIButton!
+    @IBOutlet weak var volumeSlider: UISlider!
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func pause_stream(_ sender: AnyObject) {
+        RadioPlayer.sharedInstance.pause()
+        btnPlay.setImage(UIImage(named:"btn-play"), for: UIControlState.normal)
+        btnPause.setImage(UIImage(named:"btn-pause-disabled"), for: UIControlState.normal)
+    }
+    
+    @IBAction func play_stream(_ sender: AnyObject) {
+        RadioPlayer.sharedInstance.play()
+        btnPlay.setImage(UIImage(named:"btn-play-disabled"), for: UIControlState.normal)
+        btnPause.setImage(UIImage(named:"btn-pause"), for: UIControlState.normal)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Start the music by default
+        RadioPlayer.sharedInstance.play()
+        
+        
+        // Handle music in background
+        if NSClassFromString("MPNowPlayingInfoCenter") != nil {
+            let image:UIImage = UIImage(named: "little-icon")!
+            let albumArt = MPMediaItemArtwork(image: image)
+            let songInfo = [
+                MPMediaItemPropertyTitle: "Rádio Sulamérica Paradiso",
+                MPMediaItemPropertyArtist: "FM - 95,7",
+                MPMediaItemPropertyArtwork: albumArt
+            ] as [String : Any]
+            MPNowPlayingInfoCenter.default().nowPlayingInfo = songInfo
+        }
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            UIApplication.shared.beginReceivingRemoteControlEvents()
+            print("Receiving remote control events\n")
+        } catch {
+            print("Audio Session error.\n")
+        }
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
